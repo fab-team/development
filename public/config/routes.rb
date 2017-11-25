0262b139aad68f66f0e2fe5fda5b65713098b8e3
chokosuki4400 <e.bird.onishi@gmail.com>
2017-11-25 16:53:25 +0900

    base 追加

--- public/config/routes.rb
+++ public/config/routes.rb
@@ -1,3 +1,41 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :accounts
+	resources :answers
+	resources :questions
+	### People controller ###
+
+	get 'helo/login_check'
+
+	get 'people/index'
+	get 'people', to: 'people#index'
+
+	get 'people/find'
+	post 'people/find'
+
+	get 'people/add'
+	post 'people/add', to: 'people#create'
+
+	get 'people/edit/:id', to: 'people#edit'
+	post 'people/edit/:id', to: 'people#update'
+	patch 'people/edit/:id', to: 'people#update'
+
+	get 'people/:id', to: 'people#show'
+
+	get 'people/delete/:id', to: 'people#delete'
+
+	### Dengonban controller ###
+
+	get 'dengonban', to: 'dengonban#index'
+	post 'dengonban', to: 'dengonban#index'
+	get 'dengonban/index'
+	post 'dengonban/index'
+
+	### helo controller ###
+
+	get 'helo', to: 'helo#index'
+	get 'helo/index'
+	post 'helo', to: 'helo#index'
+	post 'helo/index'
+	get 'helo/other'
 end
